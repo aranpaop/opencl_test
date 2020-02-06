c4130d55c48d50a2b83c5f0216dab54129b766ce
Chao Ye <paopstory@outlook.com>
2020-02-06 11:40:26 +0800

    update test result

--- main.cpp
+++ main.cpp
@@ -88,7 +88,7 @@ int main()
     return 0;
 }
 
-// Test result:
+// Test float result:
 /*
 Info: Successfully get platforms.
 Info: Platform 0
@@ -103,13 +103,19 @@ Info:
 __kernel void float_func(__global const float* in, __global float* out)
 {
         int tid = get_global_id(0);
-        out[tid] = in[tid * 3] * in[tid * 3 + 1] + in[tid * 3 + 2];
+        out[tid] = 0;
+        for (int i = 0; i < 10000; ++i) {
+                out[tid] += (in[tid * 3] * in[tid * 3 + 1] + in[tid * 3 + 2]) / 127.0f;
+        }
 }
 Info:
 __kernel void double_func(__global const double* in, __global double* out)
 {
         int tid = get_global_id(0);
-        out[tid] = in[tid * 3] * in[tid * 3 + 1] + in[tid * 3 + 2];
+        out[tid] = 0;
+        for (int i = 0; i < 10000; ++i) {
+                out[tid] += (in[tid * 3] * in[tid * 3 + 1] + in[tid * 3 + 2]) / 127.0;
+        }
 }
 Info: Successfully create program.
 Info: Successfully build program.
@@ -140,13 +146,9 @@ Info: Device supports denormal number of float.
 Info: Device supports denormal number of double.
 Info: Successfully create command queue.
 Info: Successfully enqueue kernel.
-Info: Time between queued and submit: 116288
-Info: Time between submit and start: 25376
-Info: Time between start and end: 7264
-Info: Successfully enqueue kernel.
-Info: Time between queued and submit: 124704
-Info: Time between submit and start: 25056
-Info: Time between start and end: 5120
+Info: Time between queued and submit: 107840
+Info: Time between submit and start: 25920
+Info: Time between start and end: 2022400
 Info: Platform 1
 Info: The name of platform is: Intel(R) OpenCL
 Info: The vendor of platform is: Intel(R) Corporation
@@ -159,13 +161,19 @@ Info:
 __kernel void float_func(__global const float* in, __global float* out)
 {
         int tid = get_global_id(0);
-        out[tid] = in[tid * 3] * in[tid * 3 + 1] + in[tid * 3 + 2];
+        out[tid] = 0;
+        for (int i = 0; i < 10000; ++i) {
+                out[tid] += (in[tid * 3] * in[tid * 3 + 1] + in[tid * 3 + 2]) / 127.0f;
+        }
 }
 Info:
 __kernel void double_func(__global const double* in, __global double* out)
 {
         int tid = get_global_id(0);
-        out[tid] = in[tid * 3] * in[tid * 3 + 1] + in[tid * 3 + 2];
+        out[tid] = 0;
+        for (int i = 0; i < 10000; ++i) {
+                out[tid] += (in[tid * 3] * in[tid * 3 + 1] + in[tid * 3 + 2]) / 127.0;
+        }
 }
 Info: Successfully create program.
 Info: Successfully build program.
@@ -196,13 +204,9 @@ Info: Device supports denormal number of float.
 Info: Device supports denormal number of double.
 Info: Successfully create command queue.
 Info: Successfully enqueue kernel.
-Info: Time between queued and submit: 213600
-Info: Time between submit and start: 3905317
-Info: Time between start and end: 6083
-Info: Successfully enqueue kernel.
-Info: Time between queued and submit: 26800
-Info: Time between submit and start: 366700
-Info: Time between start and end: 5333
+Info: Time between queued and submit: 252200
+Info: Time between submit and start: 3684133
+Info: Time between start and end: 1969833
 Info: Device 1
 Info: The name of device is: Intel(R) Core(TM) i7-8565U CPU @ 1.80GHz
 Info: The vendor of device is: Intel(R) Corporation
@@ -218,11 +222,145 @@ Info: Device supports denormal number of float.
 Info: Device supports denormal number of double.
 Info: Successfully create command queue.
 Info: Successfully enqueue kernel.
-Info: Time between queued and submit: 1800
-Info: Time between submit and start: 2127500
-Info: Time between start and end: 251500
+Info: Time between queued and submit: 1700
+Info: Time between submit and start: 2228700
+Info: Time between start and end: 2914200
+*/
+// Test double result:
+/*
+Info: Successfully get platforms.
+Info: Platform 0
+Info: The name of platform is: NVIDIA CUDA
+Info: The vendor of platform is: NVIDIA Corporation
+Info: The version of platform is: OpenCL 1.2 CUDA 10.1.236
+Info: The profile of platform is: FULL_PROFILE
+Info: Successfully get devices.
+Info: Successfully create context.
+Info: Kernel functions:
+Info:
+__kernel void float_func(__global const float* in, __global float* out)
+{
+        int tid = get_global_id(0);
+        out[tid] = 0;
+        for (int i = 0; i < 10000; ++i) {
+                out[tid] += (in[tid * 3] * in[tid * 3 + 1] + in[tid * 3 + 2]) / 127.0f;
+        }
+}
+Info:
+__kernel void double_func(__global const double* in, __global double* out)
+{
+        int tid = get_global_id(0);
+        out[tid] = 0;
+        for (int i = 0; i < 10000; ++i) {
+                out[tid] += (in[tid * 3] * in[tid * 3 + 1] + in[tid * 3 + 2]) / 127.0;
+        }
+}
+Info: Successfully create program.
+Info: Successfully build program.
+Info: Successfully create kernel.
+Info: Successfully create kernel.
+Info: Successfully create buffer.
+Info: Successfully create buffer.
+Info: Successfully set kernel arg 0
+Info: Successfully set kernel arg 1
+Info: All kernel args have been set.
+Info: Successfully create buffer.
+Info: Successfully create buffer.
+Info: Successfully set kernel arg 0
+Info: Successfully set kernel arg 1
+Info: All kernel args have been set.
+Info: Device 0
+Info: The name of device is: GeForce MX250
+Info: The vendor of device is: NVIDIA Corporation
+Info: The version of device is: OpenCL 1.2 CUDA
+Info: The profile of device is: FULL_PROFILE
+Info: The version of driver is: 426.00
+Info: The type of device is: CL_DEVICE_TYPE_GPU
+Info: The max compute units of device is: 3
+Info: The max work item dimensions of device is: 3
+Info: The max work item sizes of device are: (1024, 1024, 64, )
+Info: The max work group size of device is: 1024
+Info: Device supports denormal number of float.
+Info: Device supports denormal number of double.
+Info: Successfully create command queue.
 Info: Successfully enqueue kernel.
-Info: Time between queued and submit: 1100
-Info: Time between submit and start: 13100
-Info: Time between start and end: 50300
+Info: Time between queued and submit: 117184
+Info: Time between submit and start: 25856
+Info: Time between start and end: 9116672
+Info: Platform 1
+Info: The name of platform is: Intel(R) OpenCL
+Info: The vendor of platform is: Intel(R) Corporation
+Info: The version of platform is: OpenCL 2.1
+Info: The profile of platform is: FULL_PROFILE
+Info: Successfully get devices.
+Info: Successfully create context.
+Info: Kernel functions:
+Info:
+__kernel void float_func(__global const float* in, __global float* out)
+{
+        int tid = get_global_id(0);
+        out[tid] = 0;
+        for (int i = 0; i < 10000; ++i) {
+                out[tid] += (in[tid * 3] * in[tid * 3 + 1] + in[tid * 3 + 2]) / 127.0f;
+        }
+}
+Info:
+__kernel void double_func(__global const double* in, __global double* out)
+{
+        int tid = get_global_id(0);
+        out[tid] = 0;
+        for (int i = 0; i < 10000; ++i) {
+                out[tid] += (in[tid * 3] * in[tid * 3 + 1] + in[tid * 3 + 2]) / 127.0;
+        }
+}
+Info: Successfully create program.
+Info: Successfully build program.
+Info: Successfully create kernel.
+Info: Successfully create kernel.
+Info: Successfully create buffer.
+Info: Successfully create buffer.
+Info: Successfully set kernel arg 0
+Info: Successfully set kernel arg 1
+Info: All kernel args have been set.
+Info: Successfully create buffer.
+Info: Successfully create buffer.
+Info: Successfully set kernel arg 0
+Info: Successfully set kernel arg 1
+Info: All kernel args have been set.
+Info: Device 0
+Info: The name of device is: Intel(R) UHD Graphics 620
+Info: The vendor of device is: Intel(R) Corporation
+Info: The version of device is: OpenCL 2.1 NEO
+Info: The profile of device is: FULL_PROFILE
+Info: The version of driver is: 26.20.100.7262
+Info: The type of device is: CL_DEVICE_TYPE_GPU
+Info: The max compute units of device is: 24
+Info: The max work item dimensions of device is: 3
+Info: The max work item sizes of device are: (256, 256, 256, )
+Info: The max work group size of device is: 256
+Info: Device supports denormal number of float.
+Info: Device supports denormal number of double.
+Info: Successfully create command queue.
+Info: Successfully enqueue kernel.
+Info: Time between queued and submit: 226000
+Info: Time between submit and start: 1543750
+Info: Time between start and end: 14400250
+Info: Device 1
+Info: The name of device is: Intel(R) Core(TM) i7-8565U CPU @ 1.80GHz
+Info: The vendor of device is: Intel(R) Corporation
+Info: The version of device is: OpenCL 2.1 (Build 0)
+Info: The profile of device is: FULL_PROFILE
+Info: The version of driver is: 7.6.0.0814
+Info: The type of device is: CL_DEVICE_TYPE_CPU
+Info: The max compute units of device is: 8
+Info: The max work item dimensions of device is: 3
+Info: The max work item sizes of device are: (8192, 8192, 8192, )
+Info: The max work group size of device is: 8192
+Info: Device supports denormal number of float.
+Info: Device supports denormal number of double.
+Info: Successfully create command queue.
+Info: Successfully enqueue kernel.
+Info: Time between queued and submit: 1400
+Info: Time between submit and start: 3409900
+Info: Time between start and end: 2257200
 */
